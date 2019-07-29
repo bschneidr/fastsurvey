4f5835fb64a3bde121791b178101192dedf6cc29
tlumley <tlumley@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-07-29 20:20:07 +0000

    Remove RODBC support
    git-svn-id: svn://r-forge.r-project.org/svnroot/r-survey/pkg/survey@116 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/svyivreg.R
+++ R/svyivreg.R
@@ -5,11 +5,11 @@ svyivreg.survey.design<-function(formula, design,...){
 
     .data<-model.frame(design)
     .data$.weights<-weights(design,"sampling")
-    fitter<-get("ivreg", mode="function")
     .weights<-NULL ## make CMD check happy
-    model<- fitter(formula, data=.data, weights=.weights)
+    estfun<-get("estfun",mode="function")
+    model<- AER::ivreg(formula, data=.data, weights=.weights)
 
-    U<-estfun(model)/weights(design,"sampling")
+    U<-estfun.ivreg(model)/weights(design,"sampling")
     n<-NROW(U)
     infl<- U%*%model$cov.unscaled 
     v<-vcov(svytotal(infl,  design))
@@ -32,14 +32,29 @@ summary.svyivreg<-function(object, df = NULL, ...){
 vcov.svyivreg<-function(object,...) object$cov.unscaled
 
 svyivreg.svyrep.design<-function(formula, design,return.replicates=FALSE,...){
-    fitter<-get("ivreg", mode="function")
     .pweights<-NULL ## make CMD check happy
 
     withReplicates(design, return.replicates=return.replicates,
                    function(.weights, .data){
                        .data$.pweights<-.weights
-                       m<-fitter(formula,data= .data, weights=.pweights)
+                       m<-AER::ivreg(formula,data= .data, weights=.pweights)
                        coef(m)
                    })
                       
     }
+
+
+estfun.ivreg<-function (x, ...) 
+{
+    xmat <- model.matrix(x)
+    if (any(alias <- is.na(coef(x)))) 
+        xmat <- xmat[, !alias, drop = FALSE]
+    wts <- weights(x)
+    if (is.null(wts)) 
+        wts <- 1
+    res <- residuals(x)
+    rval <- as.vector(res) * wts * xmat
+    attr(rval, "assign") <- NULL
+    attr(rval, "contrasts") <- NULL
+    return(rval)
+}
