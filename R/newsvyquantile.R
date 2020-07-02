ac6ee60e227610dec12ea91168aeee40f0dd7953
tlumley <tlumley@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-07-02 03:03:39 +0000

    rewriting quantiles: factoring
    git-svn-id: svn://r-forge.r-project.org/svnroot/r-survey/pkg/survey@157 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/newsvyquantile.R
+++ R/newsvyquantile.R
@@ -1,91 +1,58 @@
 svyquantile<-function(x,design,quantiles,...) UseMethod("svyquantile", design)
 
-qq<-function (x, w, probs = seq(0, 1, 0.25), na.rm = FALSE, names = TRUE, 
-    type = 7, ...) 
-{
-    ## if (is.factor(x)) {
-    ##     if (is.ordered(x)) {
-    ##         if (!any(type == c(1L, 3L))) 
-    ##             stop("'type' must be 1 or 3 for ordered factors")
-    ##     }
-    ##     else stop("factors are not allowed")
-    ##     lx <- levels(x)
-    ## }
-    ## else lx <- NULL
-    if (na.rm) 
-        x <- x[!is.na(x)]
-    else if (anyNA(x)) 
-        stop("missing values and NaN's not allowed if 'na.rm' is FALSE")
-    eps <- 100 * .Machine$double.eps
-    if (any((p.ok <- !is.na(probs)) & (probs < -eps | probs > 
-        1 + eps))) 
-        stop("'probs' outside [0,1]")
-    n <- length(x)
-    if (na.p <- any(!p.ok)) {
-        o.pr <- probs
-        probs <- probs[p.ok]
-        probs <- pmax(0, pmin(1, probs))
-    }
-    np <- length(probs)
-    if (n > 0 && np > 0) {
-        if (type == 7) {
-            index <- 1 + (n - 1) * probs
-            lo <- floor(index)
-            hi <- ceiling(index)
-            x <- sort(x, partial = unique(c(lo, hi)))
-            qs <- x[lo]
-            i <- which(index > lo & x[hi] != qs)
-            h <- (index - lo)[i]
-            qs[i] <- (1 - h) * qs[i] + h * x[hi[i]]
-        }
-        else {
-            if (type <= 3) {
-                nppm <- if (type == 3) 
-                  n * probs - 0.5
-                else n * probs
-                j <- floor(nppm)
-                h <- switch(type, (nppm > j), ((nppm > j) + 1)/2, 
-                  (nppm != j) | ((j%%2L) == 1L))
-            }
-            else {
-                switch(type - 3, {
-                  a <- 0
-                  b <- 1
-                }, a <- b <- 0.5, a <- b <- 0, a <- b <- 1, a <- b <- 1/3, 
-                  a <- b <- 3/8)
-                fuzz <- 4 * .Machine$double.eps
-                nppm <- a + probs * (n + 1 - a - b)
-                j <- floor(nppm + fuzz)
-                h <- nppm - j
-                if (any(sml <- abs(h) < fuzz)) 
-                  h[sml] <- 0
-            }
-            x <- sort(x, partial = unique(c(1, j[j > 0L & j <= 
-                n], (j + 1)[j > 0L & j < n], n)))
-            x <- c(x[1L], x[1L], x, x[n], x[n])
-            qs <- x[j + 2L]
-            qs[h == 1] <- x[j + 3L][h == 1]
-            other <- (0 < h) & (h < 1) & (x[j + 2L] != x[j + 
-                3L])
-            if (any(other)) 
-                qs[other] <- ((1 - h) * x[j + 2L] + h * x[j + 
-                  3L])[other]
-        }
-    }
-    else {
-        qs <- rep(NA_real_, np)
-    }
-    if (is.character(lx)) 
-        qs <- factor(qs, levels = seq_along(lx), labels = lx, 
-            ordered = TRUE)
-    if (names && np > 0L) {
-        names(qs) <- format_perc(probs)
-    }
-    if (na.p) {
-        o.pr[p.ok] <- qs
-        names(o.pr) <- rep("", length(o.pr))
-        names(o.pr)[p.ok] <- names(qs)
-        o.pr
-    }
-    else qs
+
+
+qrule_math <-qrule_hf1 <- function(qdata){
+    if(qdata$wlow==0) qdata$qlow else qdata$qup
+}
+
+qrule_hf2 <- function(qdata){
+    if(qdata$wlow==0) (qdata$qlow+qdata$qup)/2 else qdata$qup
+}
+
+qrule_hf3 <- function(qdata){
+    if((qdata$wlow==0)&& (qdata$ilow %%2 ==0)) qdata$qlow else qdata$qup
+}
+
+
+
+
+qs <- function(x, w, p){
+    ## already has missings removed, ties handled.
+    n<-length(x)
+    ii<-order(x)
+    x<-x[ii]
+    cumw<-cumsum(w[ii])
+
+    pos<-max(which(cumw<=p*sum(w)))
+    posnext<-if(pos==length(x)) pos else pos+1
+    
+    list(qlow=x[pos], qup=x[posnext], ilow=pos,iup=posnext, wlow=cumw[pos]/sum(w)-p,wup=cumw[posnext]/sum(w)-p)
+   
+}
+    
+
+woodruff<-function(x, med, design, qrule,method=c("mean","beta","xlogit","asin"),level,df){
+
+    method<-match.arg(method)
+    m<-svymean(x<=med, design)
+
+    pconfint<-switch(method,
+                    mean=as.vector(confint(m, 1, level = level, df = df)),
+                    xlogit= {xform <- svycontrast(m, quote(log(`1`/(1 - `1`))));
+                        expit(as.vector(confint(xform, 1, level = level,  df = df)))},
+                    beta={n.eff <- coef(m) * (1 - coef(m))/vcov(m);
+                        rval <- coef(m)[1]
+                        alpha <- 1 - level
+                        n.eff <- n.eff * (qt(alpha/2, nrow(design) - 1)/qt(alpha/2, degf(design)))^2
+                        c(qbeta(alpha/2, n.eff * rval, n.eff * (1 - rval) +  1), qbeta(1 - alpha/2, n.eff * rval + 1, n.eff *  (1 - rval)))
+                    },
+                    asin={xform <- svycontrast(m, quote(asin(sqrt(`1`))))
+                        sin(as.vector(confint(xform, 1, level = level,  df = df)))^2
+                    }
+                    )
+    lower<-qrule(qs(x,weights(design,"sampling"), pconfint[1]))
+    upper<-qrule(qs(x,weights(design,"sampling"), pconfint[2]))
+    c(lower, upper)
+    
 }
