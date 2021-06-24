75afef9410061b9667b4ff568d8a87b98fd9bab4
tlumley <tlumley@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-06-24 21:44:47 +0000

    coxph/regTermTest fix
    git-svn-id: svn://r-forge.r-project.org/svnroot/r-survey/pkg/survey@206 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/regtest.R
+++ R/regtest.R
@@ -35,7 +35,7 @@ regTermTest<-function(model, test.terms, null=NULL, df=NULL, method=c("Wald","Wo
   okbeta<-!is.na(coef(model,na.rm=FALSE)) ## na.rm for svyglm
   tt<-attr(terms(model),"term.labels")
   aa<-attr(model.matrix(model),"assign")[okbeta]
-  if((inherits(model,"coxph")|| inherits(model,"svyloglin") || inherits(model,"svyolr"))  && attr(terms(model),"intercept")){
+  if((inherits(model,"svyloglin") || inherits(model,"svyolr"))  && attr(terms(model),"intercept")){
       aa<-aa[-1]
   }
     
