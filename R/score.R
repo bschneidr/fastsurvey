1a74a839b30e231a1b40e431ed5e8f1847346807
tlumley <tlumley@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-09-10 00:08:51 +0000

    score tests seem to be working
    git-svn-id: svn://r-forge.r-project.org/svnroot/r-survey/pkg/survey@235 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/score.R
+++ R/score.R
@@ -1,7 +1,6 @@
 
-fisherinf<-function(beta, model){
+fisherinf<-function(beta, model, X=model.matrix(model)){
     design<-model$survey.design
-    X<-model.matrix(model)
     eta<-X %*%beta
     mu<-drop(family(model)$linkinv(eta))
     V<-family(model)$variance(mu)
@@ -13,9 +12,8 @@ fisherinf<-function(beta, model){
 
 
 
-scores<-function(model, newbeta){
+scores<-function(model, newbeta, X=model.matrix(model)){
     design<-model$survey.design
-    X<-model.matrix(model)
     out<-newbeta==0
     eta<-X %*%newbeta
     Xin<-X[, !out, drop=FALSE]
@@ -29,7 +27,7 @@ scores<-function(model, newbeta){
     Uin<-Xin*((d/V)*(Y-mu))
     Uout<-Xout*((d/V)*(Y-mu))
     
-    I_rss98<-fisherinf(newbeta, model) ## equation 3.3, Rao, Scott, and Skinner 1998
+    I_rss98<-fisherinf(newbeta, model, X) ## equation 3.3, Rao, Scott, and Skinner 1998
     Atwiddle<-I_rss98[out,!out]%*%solve(I_rss98[!out,!out]) #equation 3.2, ibid
     Uout_in<-Uout-Uin%*%t(Atwiddle)
     
@@ -37,29 +35,31 @@ scores<-function(model, newbeta){
 }
 
 
-pseudoscore<-function(model,newbeta,ddf){
+pseudoscore<-function(model,newbeta,ddf, X=model.matrix(model)){
 
-    s<-scores(model, newbeta)
+    s<-scores(model, newbeta,X)
     np<-length(coef(s))
     Qtest<-coef(s)%*%solve(vcov(s),coef(s))
     if (is.null(ddf)) ddf<-model$df.resid
-        if (ddf<=0) {
+    if (ddf<=0) {
         warning("negative or zero ddf set to 1")
         ddf<-1
     }
+    ndf<-qr(vcov(s))$rank
+    if(ndf<np) warning(paste("df reduced from",np,"to",ndf,"due to linear dependencies"))
     if (is.finite(ddf)) {
-        p<-pf(Qtest,np,ddf,lower.tail=FALSE)
+        p<-pf(Qtest,ndf,ddf,lower.tail=FALSE)
     } else{
-       p<- pchisq(Qtest,np,lower.tail=FALSE)
+       p<- pchisq(Qtest,ndf,lower.tail=FALSE)
         }
-    c(X2=Qtest, df=np, ddf=ddf, p=p)
+    c(X2=Qtest, df=ndf, ddf=ddf, p=p)
 }
 
 
-workingscore<-function(model,newbeta,ddf, lrt.approximation){
+workingscore<-function(model,newbeta,ddf, lrt.approximation, X=model.matrix(model)){
 
-    s<-scores(model, newbeta)
-    I<-fisherinf(newbeta,model)
+    s<-scores(model, newbeta,X)
+    I<-fisherinf(newbeta,model,X)
     out<-newbeta==0
     V<-I[out,out,drop=FALSE]-I[out,!out,drop=FALSE]%*%solve(I[!out,!out,drop=FALSE],I[!out,out,drop=FALSE])
     phi<-coef(summary(model)$dispersion)
@@ -91,12 +91,31 @@ svyscoretest<-function(model, drop.terms=NULL, add.terms=NULL, method=c("working
 }
 
 
+scoretest_addterms.svyglm<-function(model,add.terms, method=method,ddf=ddf, lrt.approximation=lrt.approximation,...){
+    Xin<-model.matrix(model)
+    design<-model$survey.design
+    bigformula<-eval(bquote(update(.(formula(model)), .~.+.(add.terms[[-1]]))))
+    Xbig<-model.matrix(bigformula, model.frame(design))
+    added<-!(colnames(Xbig) %in% colnames(Xin))
+    newbeta<-rep(0,NCOL(Xbig))
+    idx<-match(colnames(Xin), colnames(Xbig))
+    newbeta[idx]=coef(model)  
+    s<-scores(model, newbeta, Xbig)
+    
+    switch(method, 
+           working=workingscore(model,newbeta, lrt.approximation=lrt.approximation,ddf=ddf, X=Xbig),
+           pseudoscore=pseudoscore(model,newbeta,ddf=ddf, X=Xbig),
+           individual=s)
+    }
+
+
 svyscoretest.svyglm<-function(model,drop.terms=NULL, add.terms=NULL, method=c("working","pseudoscore","individual"),
                               ddf=NULL,lrt.approximation="satterthwaite",...){
     if(!xor(is.null(drop.terms),is.null(add.terms))){
         stop("One of 'drop.terms' and 'add.terms' must be non-NULL")
     }
-
+    method<-match.arg(method)
+    
     canonicalOrder<-function(term){
         tt<-strsplit(term,":")
         tt<-lapply(tt,sort)
@@ -141,7 +160,7 @@ svyscoretest.svyglm<-function(model,drop.terms=NULL, add.terms=NULL, method=c("w
     beta<-coef(model)*0
     beta[-index]<-coef(model0)
 
-    switch(match.arg(method), 
+    switch(method, 
            working=workingscore(model,beta, lrt.approximation=lrt.approximation,ddf=ddf),
            pseudoscore=pseudoscore(model,beta,ddf=ddf),
            individual=scores(model, beta))
