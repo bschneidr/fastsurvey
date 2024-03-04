11a7f28e5f1b3d96008f8b02a6f1a285536800a5
tlumley <tlumley@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2024-03-04 21:50:56 +0000

    CMD check really doesn't want this test but I do
    git-svn-id: svn://r-forge.r-project.org/svnroot/r-survey/pkg/survey@316 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/testSUMMER/SUMMER.R
+++ tests/testSUMMER/SUMMER.R
@@ -1,9 +1,12 @@
+## CRAN won't allow this test unless INLA is a listed dependency
+## which is impossible, so the test is hidden away here.
 
 library(survey)
 
 data("DemoData2",package="SUMMER")
 data("DemoMap2", package="SUMMER")
 
+if(require("INLA",quietly=TRUE)){
 INLA::inla.setOption(num.threads="1:1")
  library(survey)
  des0 <- svydesign(ids = ~clustid+id, strata = ~strata,
@@ -22,3 +25,4 @@ INLA::inla.setOption(num.threads="1:1")
 print(cts.cov.res)
 plot(cts.cov.res)
 summary(cts.cov.res)
+}
